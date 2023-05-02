c8b8592045afa5be807771ab3edd48547b994128
Andrii Valenko <andreyvalenko@gmail.com>
2023-05-02 21:51:16 +0300

    eds

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,18 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { contactsReducer, filterReducer } from './index';
 
+const customMiddleware = store => {
+  return next => {
+    return action => {
+      if (typeof action === 'function') {
+        return action(store.dispatch);
+      }
+      next(action);
+    };
+  };
+};
+
 export const store = configureStore({
   reducer: { contacts: contactsReducer, filter: filterReducer },
+  middleware: [customMiddleware],
 });
